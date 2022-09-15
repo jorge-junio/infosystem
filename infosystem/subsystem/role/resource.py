0dab30853c5ccdeebd0de53b5dbc11e5461461c5
JorgeSilva <jorgejunioufrn@gmail.com>
2022-09-15 09:39:15 -0300

    Add column rle_type in role

--- infosystem/subsystem/role/resource.py
+++ infosystem/subsystem/role/resource.py
@@ -1,6 +1,14 @@
+from enum import Enum
+
+import sqlalchemy
+from infosystem.common.subsystem import entity
 from infosystem.database import db
 from sqlalchemy import UniqueConstraint
-from infosystem.common.subsystem import entity
+
+
+class RoleType(Enum):
+    MULTI_DOMAIN = 0
+    ADMIN = 1
 
 
 class Role(entity.Entity, db.Model):
@@ -9,17 +17,20 @@ class Role(entity.Entity, db.Model):
     SYSADMIN = 'Sysadmin'
     ADMIN = 'Admin'
 
-    attributes = ['name']
+    attributes = ['name', 'role_type']
     attributes += entity.Entity.attributes
 
     name = db.Column(db.String(80), nullable=False)
+    role_type = db.Column(sqlalchemy.Enum(RoleType), nullable=False,
+                          default=RoleType.ADMIN, server_default='ADMIN')
 
     __table_args__ = (
         UniqueConstraint('name', name='role_name_uk'),)
 
-    def __init__(self, id, name,
+    def __init__(self, id, name, role_type,
                  active=True, created_at=None, created_by=None,
                  updated_at=None, updated_by=None, tag=None):
         super().__init__(id, active, created_at, created_by,
                          updated_at, updated_by, tag)
         self.name = name
+        self.role_type = role_type
