bfcd8af83b88da9e51e7543e301bf612961279bd
JorgeSilva <jorgejunioufrn@gmail.com>
2022-10-05 15:37:17 -0300

    Add tag_name in Tag

--- infosystem/subsystem/tag/resource.py
+++ infosystem/subsystem/tag/resource.py
@@ -13,11 +13,14 @@ class Tag(entity.Entity, db.Model):
     name = db.Column(db.String(60), nullable=False)
     color = db.Column(db.CHAR(7), nullable=False)
     description = db.Column(db.String(1024), nullable=False)
+    tag_name = db.Column(db.String(60), nullable=False)
 
     __table_args__ = (
-        UniqueConstraint('domain_id', 'name', name='tag_domain_id_name_uk'),)
+        UniqueConstraint('domain_id', 'name', name='tag_domain_id_name_uk'),
+        UniqueConstraint('domain_id', 'tag_name',
+                         name='tag_domain_id_tag_name_uk'),)
 
-    def __init__(self, id, domain_id, name, color, description,
+    def __init__(self, id, domain_id, name, color, description, tag_name,
                  active=True, created_at=None, created_by=None,
                  updated_at=None, updated_by=None, tag=None):
         super().__init__(id, active, created_at, created_by,
@@ -26,3 +29,4 @@ class Tag(entity.Entity, db.Model):
         self.name = name
         self.color = color
         self.description = description
+        self.tag_name = tag_name
